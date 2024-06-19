696bdf127f28df64d15a4e8239ef4f7ef2f8538e
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-06-19 09:33:18 -0400

    chore: configure deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://airjp73.github.io",
+  base: "maine-go-site",
+});
