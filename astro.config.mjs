16cc0c1c525a1a6a6694c23ca40149d2b1fc6ba0
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-06-19 09:41:51 -0400

    chore: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://airjp73.github.io",
   base: "maine-go-site",
+  integrations: [tailwind()]
 });
