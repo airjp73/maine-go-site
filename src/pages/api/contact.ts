c692c26bfdd1219a3cb7028038838ed91ff95815
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-10-22 23:20:38 -0400

    feat: send email

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from "astro";
 import { z } from "zod";
 import { zfd } from "zod-form-data";
+import * as postmark from "postmark";
 
 export const prerender = false;
 
@@ -12,5 +13,27 @@ const dataSchema = zfd.formData({
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   const data = dataSchema.parse(await request.formData());
+
+  const apiKey = import.meta.env.POSTMARK_API_KEY;
+  const recipient = import.meta.env.CONTACT_FORM_RECIPIENT_EMAIL;
+  if (!apiKey || !recipient)
+    throw new Error("Postmark isn't enabled in this environment.");
+
+  const client = new postmark.ServerClient(apiKey);
+
+  await client.sendEmail({
+    From: "contact@mainego.org",
+    To: recipient,
+    Subject: "Maine Go - Contact form submission",
+    TextBody: [
+      `Name: ${data.name}`,
+      `Email: ${data.email}`,
+      "",
+      `Message:\n${data.message}`,
+    ].join("\n"),
+    MessageStream: "outbound",
+    ReplyTo: data.email,
+  });
+
   return redirect("/contact-thanks");
 };
