2aabcaecd941e334dcba95298b9f021c1214ff3b
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-10-22 23:49:04 -0400

    chore: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,12 @@ import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   site: "https://www.mainego.org/",
   integrations: [tailwind(), sitemap(), mdx()],
+  adapter: vercel(),
 });
